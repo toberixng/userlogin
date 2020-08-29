3e320a18bca2852ea3dfeb28417bc8f189074494
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-08-29 13:04:23 +0100

    feat: email validation

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -15,6 +17,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
+
+
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 // controller actions
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -7,10 +9,22 @@ module.exports.signup_get = (req, res) => {
     res.render('login');
   }
   
-  module.exports.signup_post = async (req, res) => {
-    res.send('new signup');
+module.exports.signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user)
+  } 
+  catch (err) {
+    console.log(err)
+    res.status(404).send('error, user not created')
   }
-  
-  module.exports.login_post = async (req, res) => {
-    res.send('user login');
-  }
+}
+
+module.exports.login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
+  res.send('user login');
+}
