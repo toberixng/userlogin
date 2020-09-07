5c09cd5b15a08e5a8ac7f9d69ff65bd71698d763
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-09-07 09:19:06 +0100

    feat: log in update

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,6 @@ const handleErrors = (err) => {
   if (err.message.includes('user validation failed')) {
     // console.log(err);
     Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
       errors[properties.path] = properties.message;
     });
   }
